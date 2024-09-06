f573a8ec73561b5afb6d129413ab780342641518
Ozcan Sert <ozcsert0@gmail.com>
2024-09-06 13:24:28 +0300

    [SE-7] - setup scss config

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
-import "./styles/abstracts/_variables.scss"
-import "./styles/abstracts/_mixins.scss"
-import "./styles/base/_reset.scss"
+import "./styles/styles.scss"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
