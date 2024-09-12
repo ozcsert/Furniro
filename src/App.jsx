1c32367508458b68bc666a6df2cb0bd6cb41d8be
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-09-12 20:00:35 +0300

    [SE-3] - Navbar Component edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-=======
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
->>>>>>> 8e8ac09e9ff6397ad6dbfcd0a672294eb4e525bb
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,11 @@ import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import "./styles/styles.scss"
-<<<<<<< HEAD
 import { BrowserRouter} from "react-router-dom";
 
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-     <BrowserRouter>
-      <App />
-    </BrowserRouter>
-=======
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
->>>>>>> 8e8ac09e9ff6397ad6dbfcd0a672294eb4e525bb
-  </StrictMode>
+  </BrowserRouter>
 )
