f741436ceb3c7ccc19699975fc498d749c439c47
Ozcan Sert <ozcsert0@gmail.com>
2024-09-16 15:05:17 +0300

    [SE-8] - resetted folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom"
+import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import "./styles/styles.scss"
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 )
