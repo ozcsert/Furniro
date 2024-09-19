c8e60e3f9bfff00dcb708e1f3f24d3b683ba74b8
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-09-19 12:05:12 +0300

    [SE-3] - edited main.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 import "./styles/styles.scss"
-import { BrowserRouter} from "react-router-dom";
-
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 )
