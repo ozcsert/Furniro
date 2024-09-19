6b21ccd8214c5b82521b396bb7b7f681c4e059be
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-09-19 19:34:17 +0300

    [SE-3] - hamburger menu is placed under the logo

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -12,21 +12,26 @@ const NavBar = () => {
   return (
     <div>
       <section className="navbar-section">
-        <div className="n-logo">
-          <img className="logo-img" src={navbarlogo} alt="logo" />
-        </div>
+        <div className="n-div">
+          <div className="n-logo">
+            <img className="logo-img" src={navbarlogo} alt="logo" />
+          </div>
 
-        <div className={`n-hamburger-menu ${isMenuOpen ? 'active' : ''}`}>
-          <p className="n-content">Home</p>
-          <p className="n-content">Shop</p>
-          <p className="n-content">About</p>
-          <p className="n-content">Contact</p>
-        </div>
+          <div className={`n-hamburger-menu ${isMenuOpen ? "active" : ""}`}>
+            <p className="n-content">Home</p>
+            <p className="n-content">Shop</p>
+            <p className="n-content">About</p>
+            <p className="n-content">Contact</p>
+          </div>
 
-        <div className={`hamburger-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className="bar" />
-          <div className="bar" />
-          <div className="bar" />
+          <div
+            className={`hamburger-icon ${isMenuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+          >
+            <div className="bar" />
+            <div className="bar" />
+            <div className="bar" />
+          </div>
         </div>
 
         <div className="action-component">
