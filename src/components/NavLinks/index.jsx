9cf785967bca989c98b85d919da72f0a1ab2dd84
Ozcan Sert <ozcsert0@gmail.com>
2024-09-19 20:32:37 +0300

    [SE-8] - added links inside Navlinks

---
+++ src/components/NavLinks/index.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+export const NavLinks = ({ className }) => {
+  const links = [
+    { path: "/", name: "Home" },
+    { path: "/shop", name: "Shop" },
+    { path: "/contact", name: "Contact" },
+    { path: "/about", name: "About" },
+  ];
+
+  return (
+    <ul className={className}>
+      {links.map((link, index) => (
+        <li key={index}>
+          <Link to={link.path}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
