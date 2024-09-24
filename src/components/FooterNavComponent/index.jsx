b7b51fa4c8794e8883067f9dcd57f5e901069bb1
Ozcan Sert <ozcsert0@gmail.com>
2024-09-24 20:13:26 +0300

    [SE-18] - updated component

--- src/components/FooterNavComponent/index.jsx
+++ src/components/FooterNavComponent/index.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom"
 
-const FooterNavComponent = ( title, links ) => {
+const FooterNavComponent = ({ title, links } ) => {
   return (
     <div>
       <h4>{title}</h4>
