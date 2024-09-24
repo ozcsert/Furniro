fa44820370be7ba82c56c434cbc6ecc59173ef92
onx66 <oipek01@mail.com>
2024-09-24 13:17:29 +0300

    [SE-69] - Herosection Finish

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,12 @@
-
+import Herosection from "../../components/Herosection/Herosection.jsx";
 const HomePage = () => {
   return (
     <>
       <p> Home </p>
+      <Herosection />
     </>
   )
 
 }
 
-export default HomePage;
+export default HomePage
