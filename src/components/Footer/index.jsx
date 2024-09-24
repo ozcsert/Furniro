0e61d86c8486432e723d70320f750e1494f008bf
Ozcan Sert <ozcsert0@gmail.com>
2024-09-24 15:18:16 +0300

    [SE-19] - added Footer Section

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import FooterNavComponent from "../FooterNavComponent";
+import "./styles.scss"
+
+const footerLinks = [
+  { path: "/", name: "Home" },
+  { path: "/shop", name: "Shop" },
+  { path: "/contact", name: "Contact" },
+  { path: "/about", name: "About" },
+]
+
+const helpLinks = [
+  { path: "/payment", name: "Payment Options" },
+  { path: "/returns", name: "Returns" },
+  { path: "/privacy", name: "Privacy Policy" },
+]
+
+
+const Footer = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Email submitted:', email);
+  };
+  
+  return(
+    <footer>
+      <h2 >Furniro.</h2>
+      <section>
+        <FooterNavComponent title="LINKS" links={footerLinks}/>
+        <FooterNavComponent title="HELP" links={helpLinks}/>
+        <div className="ftr__location">
+          <h4>LOCATION</h4>
+          <p>400 University Drive Suite 200 Coral Gables,
+          </p>
+          <p>FL 33134 USA</p>
+        </div>
+        <div >
+          <h4>NEWSLETTER</h4>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter Your Email Address"
+              required
+            />
+            <button type="submit">SUBSCRIBE</button>
+          </form>
+        </div>
+      </section>
+      <div className="ftr__bttm-info">
+        <p>2023 Furniro. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;  
