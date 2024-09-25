5a507c989b2ed454f5b2c221b96c1144245d4e1b
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-09-25 15:53:31 +0300

    [SE-26] - SPHS start

--- src/components/BuyNowButton/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.scss";
-
-const BuyNowButton = () => {
-  return <button className="buy-now-button">BUY NOW</button>;
-};
-
-export default BuyNowButton;
