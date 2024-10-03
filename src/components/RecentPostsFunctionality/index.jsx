a89e8ab0a986e23d527d78f4acb86a210aa5bd7d
Necmi Gunduz <necmigunduz@gmail.com>
2024-10-03 20:27:28 +0300

    [SE-25] - fix responsiveness and move data out to a sep file

--- src/components/RecentPostsFunctionality/index.jsx
+++ src/components/RecentPostsFunctionality/index.jsx
@@ -1,39 +1,7 @@
 import "../RecentPostsFunctionality/style.scss";
-import recentPostsimg1 from "../../assets/images/recentPostsimg1.png";
-import recentPostsimg2 from "../../assets/images/recentPostsimg2.png";
-import recentPostsimg3 from "../../assets/images/recentPostsimg3.png";
-import recentPostsimg4 from "../../assets/images/recentPostsimg4.png";
-import recentPostsimg5 from "../../assets/images/recentPostsimg5.png";
-
-
+import { news } from "./data";
 
 const RecentPostsFunctionality = () => {
-    const news = [{
-        img: recentPostsimg1,
-        text: "Going all-in with millennial design",
-        date: "13 Jun 2024",
-    },
-    {
-        img: recentPostsimg2,
-        text: "Exploring new ways of decorating",
-        date: "22 Aug 2024",
-    },
-    {
-        img: recentPostsimg3,
-        text: "Handmade pieces that took time to make",
-        date: "02 Jun 2024",
-    },
-    {
-        img: recentPostsimg4,
-        text: "Modern home in Milan",
-        date: "05 May 2024",
-    },
-    {
-        img: recentPostsimg5,
-        text: "Colorful office redesign",
-        date: "26 Sep 2024",
-    },
-    ]
     return (
         <div className="recent-post-main">
             <div className="recent-post-include">
