c34aaf43fe8f0c9c0a3b3ad31b8030c5ac459c08
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-10-05 18:17:12 +0300

    [SE-10] - BC colors add

--- src/components/FooterNavComponent/index.jsx
+++ src/components/FooterNavComponent/index.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
-const FooterNavComponent = ({ title, links } ) => {
-=======
 const FooterNavComponent = ({ title, links }) => {
->>>>>>> develop
   return (
     <div>
       <h4>{title}</h4>
@@ -17,11 +13,7 @@ const FooterNavComponent = ({ title, links }) => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
-export default FooterNavComponent
+export default FooterNavComponent;
