30b9e21567a80ce1da59793f5ede2e7bda20382e
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-10-05 19:32:56 +0300

    [SE-21] - Fc card setting add

--- src/components/FeatureCard/index.jsx
+++ src/components/FeatureCard/index.jsx
@@ -1,14 +1,45 @@
 import React from "react";
+import trophyIcon from "../../../public/trophy.svg";
+import guaranteeIcon from "../../../public/guarantee.svg";
+import shippingIcon from "../../../public/shipping.svg";
+import supportIcon from "../../../public/customer-support.svg";
 import "./style.scss";
 
-const FeatureCard = ({ icon, title, description }) => {
+// Her bir kartı temsil eden ayrı bir bileşen
+const Card = ({ icon, title, description }) => {
+  return (
+    <div className="card">
+      <img src={icon} alt={title} className="card-icon" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+// Kapsayıcı bileşen
+const FeatureCard = () => {
   return (
     <div className="feature-card">
-      <img src={icon} alt={title} className="feature-card__icon" />
-      <div className="feature-card__content">
-        <h3 className="feature-card__title">{title}</h3>
-        <p className="feature-card__description">{description}</p>
-      </div>
+      <Card
+        icon={trophyIcon}
+        title="Yüksek Kalite"
+        description="En iyi malzemelerden üretilmiştir"
+      />
+      <Card
+        icon={guaranteeIcon}
+        title="Garanti Koruması"
+        description="2 yıldan fazla"
+      />
+      <Card
+        icon={shippingIcon}
+        title="Ücretsiz Kargo"
+        description="150 $ üzeri siparişler"
+      />
+      <Card
+        icon={supportIcon}
+        title="7/24 Destek"
+        description="Özel müşteri desteği"
+      />
     </div>
   );
 };
