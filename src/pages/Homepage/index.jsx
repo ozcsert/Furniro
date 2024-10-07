606d501800fdf226253a08729ce3d86482772361
Ozcan Sert <ozcsert0@gmail.com>
2024-10-07 15:50:00 +0300

    [SE-29] - fixed repo

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import Herosection from "../../components/Herosection"
 
-<<<<<<< HEAD
 const Homepage = () => {
   return (
     <div>
@@ -10,22 +8,3 @@ const Homepage = () => {
   )
 }
 export default Homepage
-=======
-=======
-import Herosection from "../../components/Herosection/index.jsx";
->>>>>>> 4954805 ([SE-29] - restored hero import)
-const HomePage = () => {
-  return (
-    <>
-      <p> Home </p>
-    </>
-  )
-
-}
-
-<<<<<<< HEAD
-export default HomePage
->>>>>>> d666ef6 ([SE-29] - fix2)
-=======
-export default HomePage
->>>>>>> 4954805 ([SE-29] - restored hero import)
