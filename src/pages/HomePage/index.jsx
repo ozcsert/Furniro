47805c6dd3855456ce9444d6dca563c9e00f9552
yayakup <yayakup66@gmail.com>
2024-10-07 20:30:42 +0300

    [SE-9] -  ana sayfadan component kaldirildi.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,11 +2,9 @@ import FilterComponent from "../../components/Filter/index";
 const HomePage = () => {
   return (
     <>
-    <FilterComponent/>
       <p> Home </p>
     </>
-  )
+  );
+};
 
-}
-
-export default HomePage
+export default HomePage;
