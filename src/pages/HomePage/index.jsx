810062e5dd9f605b75cfbd5f99559c6b82e4e2a0
yayakup <yayakup66@gmail.com>
2024-10-07 21:02:17 +0300

    [SE-9] -  style eklemesi yapildi.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,7 +2,11 @@ import Herosection from "../../components/Herosection/index.jsx"
 const HomePage = () => {
   return (
     <>
+<<<<<<< HEAD
       <FilterComponent />
+=======
+    <FilterComponent/>
+>>>>>>> 3aed774 ([SE-9] -  style eklemesi yapildi.)
       <p> Home </p>
     </>
   )
