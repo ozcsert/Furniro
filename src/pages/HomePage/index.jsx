3ba31003a49a938cdb038097722402eb5969664a
Ozcan Sert <ozcsert0@gmail.com>
2024-10-07 20:45:34 +0300

    [SE-29] - solved merge conflicts

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import Herosection from "../../components/Herosection/index.jsx"
+
+const HomePage = () => {
+  return (
+    <>
+      <p> Home </p>
+      <Herosection />
+    </>
+  )
+}
+
+export default HomePage
