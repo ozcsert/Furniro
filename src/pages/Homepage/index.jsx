69018bf7389918cb1676a15fd96aa895a33360df
Ozcan Sert <ozcsert0@gmail.com>
2024-10-07 15:49:23 +0300

    [SE-29] - rebasing

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Herosection from "../../components/Herosection"
 
 <<<<<<< HEAD
@@ -10,13 +11,21 @@ const Homepage = () => {
 }
 export default Homepage
 =======
+=======
+import Herosection from "../../components/Herosection/index.jsx";
+>>>>>>> 4954805 ([SE-29] - restored hero import)
 const HomePage = () => {
   return (
     <>
-      <Herosection />
+      <p> Home </p>
     </>
   )
+
 }
 
+<<<<<<< HEAD
 export default HomePage
 >>>>>>> d666ef6 ([SE-29] - fix2)
+=======
+export default HomePage
+>>>>>>> 4954805 ([SE-29] - restored hero import)
