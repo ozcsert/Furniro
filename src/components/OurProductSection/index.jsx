f1306072260139b65cbecd2fc068701149693e07
ygttncy <yigit03yigit@gmail.com>
2024-10-07 20:52:37 +0300

    [SE-14] - created productcard.jsx

--- src/components/OurProductSection/index.jsx
+++ src/components/OurProductSection/index.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import "./index.scss";
-import StarRating from "../StarRating/StarRating.jsx";
+import ProductCard from "./ProductCard";
 
-const ProductCard = () => {
+const ProductsCard = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -10,47 +9,25 @@ const ProductCard = () => {
   useEffect(() => {
     fetch("/public/data/sample.json")
       .then((response) => {
-        console.log("Response Status:", response.status); 
+        console.log("Response Status:", response.status);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         return response.json();
       })
       .then((data) => {
-        console.log("Fetched Data:", data); 
+        console.log("Fetched Data:", data);
         setProducts(data);
         setLoading(false);
       })
       .catch((err) => {
-        console.error("Fetch Error:", err); 
+        console.error("Fetch Error:", err);
         setError("An error occurred while loading the data.");
         setLoading(false);
       });
   }, []);
-  
 
-  const ProductCard = ({ product }) => {
-    return (
-      <div className="product-card">
-        {product.discount ? (
-          <span className="new-discount">{product.discount}%</span>
-        ) : (
-          <span className="new-label">NEW</span>
-        )}
-
-        <img src={product.image} alt={product.name} className="product-image" />
-        <h2 className="product-name">{product.name}</h2>
-        <p className="product-info">{product.description}</p>
-
-        <StarRating rating={product.rating} />
-
-        <div className="product-price">
-          <span className="discounted-price">${product.price}</span>
-          <span className="original-price">${product.originalPrice}</span>
-        </div>
-      </div>
-    );
-  };
+  <ProductCard />
 
   return (
     <div className="product-container">
@@ -69,4 +46,4 @@ const ProductCard = () => {
   );
 };
 
-export default ProductCard;
+export default ProductsCard
