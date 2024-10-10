5d8b9828f0334fcec7154773e5a7bcd09b367566
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-10-10 23:03:29 +0300

    [SE-10] - BC only buttons are combined

---
+++ src/components/ButtonComponent/GeneralButtons/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./style.scss";
+
+const GeneralButtons = ({
+  btnName = "Button",
+  width = "auto",
+  height = "auto",
+  backgroundColor = "#b88e2f",
+  fontSize = "16px",
+  borderColor = "#b88e2f",
+  textColor = "#b88e2f",
+}) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <button
+      className={`generalButton ${isActive ? "active" : ""}`}
+      style={{
+        width,
+        height,
+        backgroundColor: isActive ? "#ff9900" : backgroundColor,
+        fontSize,
+        border: `2px solid ${borderColor}`,
+        color: textColor,
+      }}
+      onClick={() => setIsActive(!isActive)}
+    >
+      {btnName}
+    </button>
+  );
+};
+
+export default GeneralButtons;
