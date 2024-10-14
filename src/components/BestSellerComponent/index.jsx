eb426aca7e2a6b7c59f054540a0108123fc36a6c
ygttncy <yigit03yigit@gmail.com>
2024-10-14 20:45:17 +0300

    [SE-13] - Responsive düzeltme ve yöneldirme ekleme

--- src/components/BestSellerComponent/index.jsx
+++ src/components/BestSellerComponent/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import StarRating from "../StarRating/StarRating.jsx";
 import "./style.scss";
+import { Link } from "react-router-dom";
 
 const BestSellers = () => {
   const [products, setProducts] = useState([]);
@@ -22,25 +23,27 @@ const BestSellers = () => {
       <h2>Best Sellers</h2>
       <div className="clothes">
         {products.map((product) => (
-          <div className="product" key={product.id}>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <div className="product-info">
-              <h3>{product.name}</h3>
-              <StarRating rating={product.rating} />
-              <div className="price-info">
-                <span className="original-price">
-                  ${product.originalPrice.toFixed(2)}
-                </span>
-                <span className="current-price">
-                  ${product.price.toFixed(2)}
-                </span>
+          <Link to="#" key={product.id}>
+            <div className="product">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <div className="product-info">
+                <h3>{product.name}</h3>
+                <StarRating rating={product.rating} />
+                <div className="price-info">
+                  <span className="original-price">
+                    ${product.originalPrice.toFixed(2)}
+                  </span>
+                  <span className="current-price">
+                    ${product.price.toFixed(2)}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
