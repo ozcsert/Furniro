8968ee49bd1f0632e1d5711ec4a34e9f0eec3258
ygttncy <yigit03yigit@gmail.com>
2024-10-25 23:11:16 +0300

    [SE-6] - Explore Card Component completed

--- src/components/ExpoloreCardComponent/index.jsx
+++ src/components/ExpoloreCardComponent/index.jsx
@@ -1,31 +1,45 @@
 import "./style.scss";
 import { Link } from "react-router-dom";
-const ExploreCardComponent = () => {
+
+import arrow from "../../assets/arrow.svg";
+import bedroom from "../../assets/bedroom.png";
+import vector from "../../assets/Vector-1.svg";
+import bedroom2 from "../../assets/Rectangle25.png"
+import bedroom3 from "../../assets/Rectangle26.png"
+
+const ExploreCard = () => {
+  const images = [bedroom, bedroom2, bedroom3];
+
   return (
     <div className="explore-card-wrapper">
-      <div className="products-wrapper">
-        <img
-          src="src/assets/bedroom.png"
-          alt="bedroom"
-          className="product-image"
-        />
-        <div className="info-section">
-          <div className="info-top">
-            <h6>01 Bed Room</h6>
-          </div>
-          <div className="info-bottom">
-            <h3>Inner Peace </h3>
-          </div>
-        </div>
+      {images.map((image, index) => (
+        <div className="products-wrapper" key={index}>
+          <img src={image} alt={`bedroom-${index + 1}`} className="product-image" />
 
-        <div className="redirect-button">
-          <Link to="#">
-            <img src="src/assets/arrow.svg" alt="Arrow" />
-          </Link>
+          <div className="info-container">
+            <div className="info-section">
+              <div className="info-top">
+                <h6>
+                  <span>{`0${index + 1}`}</span>
+                  <img src={vector} alt="vector divider" className="vector-divider" />
+                  <span>Bed Room</span>
+                </h6>
+              </div>
+              <div className="info-bottom">
+                <h3>Inner Peace</h3>
+              </div>
+            </div>
+
+            <div className="redirect-button">
+              <Link to="#">
+                <img src={arrow} alt="Arrow" />
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
 
-export default ExploreCardComponent;
+export default ExploreCard;
