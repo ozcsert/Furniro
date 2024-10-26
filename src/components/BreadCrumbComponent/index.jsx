dde872078f3cb6d8a6deb3076cf8cbf6254bb419
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-10-26 18:14:39 +0300

    [SE-23] - added

---
+++ src/components/BreadCrumbComponent/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const BreadCrumbComponent = () => {
+  return (
+    <Link to="/" className="home-link">
+      Home
+    </Link>
+  );
+};
+
+export default BreadCrumbComponent
