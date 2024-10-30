0393b58a62eae5a2febf89fbca16a099bb21728c
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-10-30 18:33:46 +0300

    [SE-20] - pathname edited

--- src/components/HeaderBanner/index.jsx
+++ src/components/HeaderBanner/index.jsx
@@ -1,24 +1,20 @@
 import "./style.scss";
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import bannerLogo from "../../assets/images/bannerLogo.png";
-import right from "../../assets/images/right.png";
 import BreadCrumbComponent from "../BreadCrumbComponent";
 
-const HeaderBanner = ({
-  title = "Product Comparison",
-  name = "Product Comparison",
-}) => {
+const HeaderBanner = () => {
+  const location = useLocation();
+  const pageTitle = location.pathname.split("/").filter(Boolean).pop() || "Home";
+
   return (
     <div className={`banner-container`}>
       <div className="banner-content">
         <img src={bannerLogo} alt="logo" className="banner-logo" />
-        <h1>{title}</h1>
-        <nav className="banner-navigate">
-          <BreadCrumbComponent/>
-          <img src={right} alt="right-img" />
-          <span className="banner-pagename">{name}</span>
-        </nav>
+        <h1>{pageTitle}</h1>
+        <BreadCrumbComponent />
       </div>
     </div>
   );
