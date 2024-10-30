7c366d7326c08bfbc890d195aa5aeadf8133509e
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-10-30 18:37:59 +0300

    [SE-23] - breadcrumb edited

--- src/components/BreadCrumbComponent/index.jsx
+++ src/components/BreadCrumbComponent/index.jsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import "./style.scss";
+import { Link, useLocation } from "react-router-dom";
+import right from "../../assets/images/right.png";
 
 const BreadCrumbComponent = () => {
+  const location = useLocation();
+  const pageName = location.pathname.split("/").filter(Boolean).pop() || "Home";
+
   return (
-    <Link to="/" className="home-link">
-      Home
-    </Link>
+    <nav className="banner-navigate">
+      <Link to="/" className="home-link">
+        Home
+      </Link>
+      <img src={right} alt="right-img" />
+      <span className="banner-pagename">{pageName}</span>
+    </nav>
   );
 };
 
-export default BreadCrumbComponent
+export default BreadCrumbComponent;
