629b70d13cebf1274fb73b62a50cb3a225138480
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-11-06 17:04:44 +0300

    [SE-49] - added marginBottom to FeatureCard

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -4,11 +4,12 @@ import FeatureCard from "../../components/FeatureCard";
 const ContactPage = () => {
   return (
     <>
-      <ContactPageComponent/>
-      <FeatureCard/>
+      <ContactPageComponent />
+      <div style={{ marginBottom: "80px" }}>
+        <FeatureCard />
+      </div>
     </>
-  )
-
-}
+  );
+};
 
 export default ContactPage;
