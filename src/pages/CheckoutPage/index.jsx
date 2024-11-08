ce23ae80a363ded50bbab5288d2f38ef61aa9ba8
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-11-08 20:20:54 +0300

    [SE-46] - added

--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -1,9 +1,15 @@
 import HeaderBanner from "../../components/HeaderBanner";
+import ProductCheckoutDetailsComponent from "../../components/ProductCheckoutDetailsComponent";
+import FeatureCard from "../../components/FeatureCard";
 
 const CheckoutPage = () => {
   return (
     <>
       <HeaderBanner/>
+      <ProductCheckoutDetailsComponent/>
+      <div style={{ marginBottom: "80px" }}>
+        <FeatureCard />
+      </div>
     </>
   )
 
