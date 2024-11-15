f57b981471a56a66e68ccd9802f8a6a86d379e4d
onx66 <oipek01@mail.com>
2024-11-15 17:04:29 +0300

    [SE-38] - Cart-Page

--- src/components/CartPageDesign/CartPageDesignChild/index.jsx
+++ src/components/CartPageDesign/CartPageDesignChild/index.jsx
@@ -1,36 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './style.scss'
 import { Link } from 'react-router-dom'
 import icondust from '../../../assets/icons/delete.svg'
-
+import { useNavigate } from "react-router-dom"
 
 
 
 
 const CartPageDesignChild = () => {
+    const navigate = useNavigate()
+    const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([
-        { id: 1, name: 'Asgaard sofa', price: 3500, img: "https://www.scandesigns.com/wp-content/uploads/2022/10/Safford-Sofa-Grey-Front-Featured--600x420.jpg" },
-        { id: 2, name: 'Asgaard chair', price: 1500, img: "https://wakefitdev.gumlet.io/img/npl_modified_images/darcy/sofa_WLCHRDRCFVBL/sofa_WLCHRDRCFVBL_1.jpg?w=640" },
-        { id: 3, name: 'Asgaard desk', price: 1000, img: "https://m.media-amazon.com/images/I/71scnKP+LuL._AC_SY300_SX300_.jpg" },
-        { id: 4, name: 'Asgaard tv unit', price: 2000, img: "https://img.vivense.com/1920x1280/images/ac6748071e7844fa8eb2c3ac9b5833ca.jpg" },
-        { id: 5, name: 'Rixos tv sofa', price: 6000, img: "https://img.vivense.com/1920x1280/images/ac6748071e7844fa8eb2c3ac9b5833ca.jpg" },
-        { id: 6, name: 'Rixos tv unit', price: 3000, img: "https://img.vivense.com/1920x1280/images/ac6748071e7844fa8eb2c3ac9b5833ca.jpg" },
-        { id: 7, name: 'Rixos tv unit', price: 3700, img: "https://img.vivense.com/1920x1280/images/ac6748071e7844fa8eb2c3ac9b5833ca.jpg" },
-        { id: 8, name: 'Rixos tv unit', price: 1250, img: "https://img.vivense.com/1920x1280/images/ac6748071e7844fa8eb2c3ac9b5833ca.jpg" },
-    ]);
+    useEffect(() => {
+
+        const cartData = localStorage.getItem("cart");
+        if (cartData) {
+            try {
+                const parsedData = JSON.parse(cartData);
+                setProducts(parsedData);
+            } catch (error) {
+                console.error("Cart verisi parse edilemedi:", error);
+            }
+        }
+    }, []);
+    console.log(products);
 
     const deleteProduct = (id) => {
         setProducts(products.filter(product => product.id !== id));
+        localStorage.setItem("cart",JSON.stringify(products));
     };
-
+    const handleDetail = (productId) => {
+        navigate(`/product/${productId}`)
+    }
 
     return (
         <ul className='cart-page-design-container'>
             {products.map((product) => (
                 <>
                     <li className='cart-page-design-section'>
-                        <img className='cart-page-design-section-img' src={product.img} alt="productimg" width={105} height={105} />
+                        <img className='cart-page-design-section-img' onClick={() => {
+                            handleDetail(product.id)
+                        }} src={product.images[0]} alt="productimg" width={105} height={105} />
                         <div className='cart-page-design-section-info'>
                             <Link to="/#" className='cart-page-design-section-info'>
                                 <p className='cart-page-design-section-name'>{product.name}</p>
