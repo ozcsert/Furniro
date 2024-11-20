9f77651351253159168f479f49a0c4f74aed84c1
ORHAN0006 <orhnbykyvz06@gmail.com>
2024-11-20 18:32:45 +0300

    [SE-26] - SP page add

--- src/pages/SingleProductPage/index.jsx
+++ src/pages/SingleProductPage/index.jsx
@@ -1,12 +1,15 @@
 import { Tab } from "../../components/Tab";
 import { useParams } from "react-router-dom";
-
+import SingleProduct from "../../components/SingleProduct/index";
 const SingleProductPage = () => {
   const { id } = useParams();
 
   return (
     <>
       <p>Product</p>
+      <div>
+        <SingleProduct />
+      </div>
       <Tab id={id} />
     </>
   );
