bd4d32e8e500854f0622cd157c0148a68528f70c
SevvalMarasli <svvlmarasli7500@gmail.com>
2024-11-23 19:05:55 +0300

    [SE-53] - added

--- src/pages/BlogPage/index.jsx
+++ src/pages/BlogPage/index.jsx
@@ -11,33 +11,50 @@ const BlogPage = () => {
   return (
     <>
       <HeaderBanner />
-      <div className="blog-post-list">
-        <BlogPostComponent
-          coverImage={image1}
-          author="Admin"
-          date="14 Oct 2022"
-          category="Wood"
-          title="Going all-in with millennial design"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
-        />
-        <BlogPostComponent
-          coverImage={image2}
-          author="Admin"
-          date="14 Oct 2022"
-          category="Handmade"
-          title="Exploring new ways of decorating"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
-        />
-        <BlogPostComponent
-          coverImage={image3}
-          author="Admin"
-          date="14 Oct 2022"
-          category="Wood"
-          title="Handmade pieces that took time to make"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
-        />
+      <div className="container-div">
+        <div className="top-div">
+          <div className="blog-post-list">
+            <BlogPostComponent
+              coverImage={image1}
+              author="Admin"
+              date="14 Oct 2022"
+              category="Wood"
+              title="Going all-in with millennial design"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
+            />
+            <BlogPostComponent
+              coverImage={image2}
+              author="Admin"
+              date="14 Oct 2022"
+              category="Handmade"
+              title="Exploring new ways of decorating"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
+            />
+            <BlogPostComponent
+              coverImage={image3}
+              author="Admin"
+              date="14 Oct 2022"
+              category="Wood"
+              title="Handmade pieces that took time to make"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
+            />
+          </div>
+
+          <div className="right-div">
+            <div className="filter-div">
+              <p>FİLTER DİV</p>
+            </div>
+
+            <div className="resent-post-div">
+              <RecentPostsFunctionality />
+            </div>
+          </div>
+        </div>
+
+        <div className="page-div">
+          <p>PAGE DİV</p>
+        </div>
       </div>
-      <RecentPostsFunctionality />
       <FeaturesSection />
     </>
   );
