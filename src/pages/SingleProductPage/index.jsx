d4fbfeecdb1f83d6434481ce95190f4f2083eab3
yayakup <yayakup66@gmail.com>
2024-12-01 08:51:09 +0300

    [SE-32] - scss duzenlemesi yapildi

--- src/pages/SingleProductPage/index.jsx
+++ src/pages/SingleProductPage/index.jsx
@@ -2,7 +2,6 @@ import ProductDetails from "../../components/SingleProductDetails"
 import { Tab } from "../../components/Tab"
 import { useParams } from "react-router-dom"
 import "./style.scss"
-import HeaderBanner from "../../components/HeaderBanner"
 import ProductNavigation from "../../components/SingleProductNavigation"
 const SingleProductPage = ({subBreadCrumbs}) => {
   const { id } = useParams()
@@ -10,7 +9,6 @@ const SingleProductPage = ({subBreadCrumbs}) => {
   return (
     <div>
       <div>
-        <HeaderBanner />
         <ProductNavigation id={id} type={subBreadCrumbs} />
       </div>
 
