e34b5d613a72a3185370e5f65b7a2f22f2460e75
Ozcan Sert <ozcsert0@gmail.com>
2024-12-05 16:54:24 +0300

    [SE-00] - updated: build config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./", // Use this for relative paths
+  base: "./",
   build: {
-    outDir: "dist", // Customize if necessary
+    outDir: "dist",
   },
   plugins: [react(), svgr()],
 })
